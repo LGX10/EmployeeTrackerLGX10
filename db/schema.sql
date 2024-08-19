55e6f4db6eee8871c551fdc3dc4ab20847b1544f
CRbukoski <camerorb45@gmail.com>
2024-08-19 13:38:34 -0400

    added code to seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
 DROP DATABASE IF EXISTS tracker_db;
-CREATE DATABASE IF EXISTS tracker_db;
+CREATE DATABASE IF NOT EXISTS tracker_db;
 
 \c tracker_db;
 
 CREATE TABLE department (
-id SERIAL PRIMARY KEY
-name VARCHAR(30) NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
-id SERIAL PRIMARY KEY
-title VARCHAR(30) NOT NULL
-salary DECIMAL(10,2) NOT NULL
-FOREIGN KEY (department_id)
-REFERENCES department(id)
-ON DELETE SET NULL
-)
+CREATE TABLE role_names (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
-id SERIAL PRIMARY KEY
-first_name VARCHAR(30) NOT NULL
-last_name VARCHAR(30) NOT NULL
-FOREIGN KEY (role_id)
-REFERENCES role(id)
-ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role_names(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO departnment (name)
+VALUES ('Human Resources'),
+       ('Accounting'),
+       ('Marketing'),
+       ('Sales Rep'),
+       ('Software Engineering'),
+       ('Manager');
+
+INSERT INTO role_names (name, department_id)
+VALUES ('')
